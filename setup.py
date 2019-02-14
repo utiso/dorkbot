18dcb0fab6377de54c9873741babea4d6076b1dd
jgor <jgor@indiecom.org>
2019-02-14 15:23:03 -0600

    restructure as a package

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dorkbot",
+    version="0.0.1",
+    author="jgor",
+    author_email="jgor@utexas.edu",
+    description="Command-line tool to scan search results for vulnerabilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://dorkbot.io",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        "console_scripts": ["dorkbot=dorkbot.dorkbot:main"],
+    },
+    include_package_data=True,
+)
+
