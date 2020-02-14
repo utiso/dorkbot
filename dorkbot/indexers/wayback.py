998526f5d10c50cc5eac0645db9b5591db288190
jgor <jgor@indiecom.org>
2020-02-14 12:32:02 -0600

    update wayback with commoncrawl enhancements

--- dorkbot/indexers/wayback.py
+++ dorkbot/indexers/wayback.py
@@ -1,9 +1,13 @@
 from urllib.request import urlopen
 from urllib.parse import urlencode, urlparse
 from urllib.error import HTTPError
+from concurrent.futures import ThreadPoolExecutor
+from itertools import repeat
 import json
 import sys
+import re
 import logging
+import time
 
 def run(options):
     required = ["domain"]
@@ -12,6 +16,8 @@ def run(options):
             logging.error("%s must be set", r)
             sys.exit(1)
 
+    retries = int(options.get("retries", "10"))
+    threads = int(options.get("threads", "1"))
     domain = options["domain"]
     time_from = options.get("from", "")
     time_to = options.get("to", "")
@@ -27,49 +33,98 @@ def run(options):
         data["from"] = time_from
     if time_to:
         data["to"] = time_to
+
+    num_pages = get_num_pages(data, retries)
  
-    results = get_results(data, domain)
+    results = get_results(domain, data, num_pages, threads, retries)
     for result in results:
         logging.debug(result)
     logging.info("Fetched %d results", len(results))
     return results
 
-def get_num_pages(data):
+def get_num_pages(data, retries):
     data["showNumPages"] = "true"
     url = "https://web.archive.org/cdx/search/cdx?" + urlencode(data)
 
-    try:
-        response = urlopen(url)
-        response = response.read().decode("utf-8")
-    except HTTPError as e:
-        logging.error("Failed to fetch number of pages - %s", str(e))
-        sys.exit(1)
+    for i in range(retries):
+        try:
+            response = urlopen(url)
+            response = response.read().decode("utf-8")
+        except HTTPError as e:
+            if e.code == 504 or e.code == 503 or e.code == 404 or e.code == 403:
+                if i == retries - 1:
+                    logging.error("Failed to fetch number of pages (retries exceeded) - %s", str(e))
+                    sys.exit(1)
+                else:
+                    logging.warn("Failed to fetch number of pages (will retry) - %s", str(e))
+                    time.sleep(1)
+                continue
+            else:
+                raise
+        except Exception:
+            logging.exception("Failed to fetch number of pages")
+            sys.exit(1)
+        break
 
+    del data["showNumPages"]
     num_pages = int(response)
+    logging.debug("Got %d pages", num_pages)
     return num_pages
 
-def get_results(data, domain):
-    num_pages = get_num_pages(data)
-    del data["showNumPages"]
+def get_page(domain, data, retries, page):
+    data["page"] = page
+    url = "https://web.archive.org/cdx/search/cdx?" + urlencode(data)
 
-    results = []
-    for page in range(0, num_pages):
-        data["page"] = page
-        url = "https://web.archive.org/cdx/search/cdx?" + urlencode(data)
+    logging.debug("Fetching page %d", page)
+    for i in range(retries):
         try:
             response_str = urlopen(url)
             response_str = response_str.read().decode("utf-8")
             response = json.loads(response_str)
         except HTTPError as e:
-            logging.error("Failed to fetch results - %s", str(e))
+            if e.code == 504 or e.code == 503 or e.code == 404 or e.code == 403:
+                if i == retries - 1:
+                    logging.error("Failed to fetch results (page %d, retries exceeded) - %s", page, str(e))
+                    sys.exit(1)
+                else:
+                    logging.warn("Failed to fetch results (page %d, will retry) - %s", page, str(e))
+                    time.sleep(1)
+                continue
+            else:
+                raise
+        except Exception:
+            logging.exception("Failed to fetch results (page %d)", page)
             sys.exit(1)
+        break
 
-        for item in response[1:]:
-            parsed = urlparse(item[0].strip())
-            if parsed.hostname.endswith(domain):
-                parsed = parsed._replace(netloc=parsed.hostname)
-                url = parsed.geturl()
-                results.append(url)
+    pattern = "http[s]?://([^/]*\.)*" + domain + "/"
+    domain_url = re.compile(pattern)
+
+    results = set()
+    for item in response[1:]:
+        url_parsed = urlparse(item[0].strip())
+        url_parsed = url_parsed._replace(netloc=url_parsed.hostname)
+        url = url_parsed.geturl()
+        if domain_url.match(url):
+            results.add(url)
 
     return results
+
+def get_results(domain, data, num_pages, threads, retries):
+    try:
+        executor = ThreadPoolExecutor(max_workers=threads)
+        threads = executor.map(get_page, repeat(domain), repeat(data), repeat(retries), range(num_pages))
+    except Exception:
+        logging.error("Failed to execute threads")
+        sys.exit(1)
+
+    results = set()
+    try:
+        for result in list(threads):
+            results.update(result)
+    except Exception:
+        logging.exception("Failed to fetch all results")
+        sys.exit(1)
+
+    return list(results)
 
